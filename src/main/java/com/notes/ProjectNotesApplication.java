f81ceac33170387669ff05a2c2e5b50bbbd46880
khayalfarzi <xeyalferziyev9@gmail.com>
2020-01-15 10:08:24 +0300

    creating project

---
+++ src/main/java/com/notes/ProjectNotesApplication.java
@@ -0,0 +1,13 @@
+package com.notes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectNotesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectNotesApplication.class, args);
+    }
+
+}
